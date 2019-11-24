606cdbe6c6213bff42dd54ce0a5bf26b7b5582fc
jéssica senger <jessicasbogoni1@gmail.com>
2019-11-24 00:35:39 -0300

    Momento que criamos as rotas
    Várias coisa no meio o caminho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ResultadoComponent } from './resultado/resultado.component';
+import { EscolaComponent } from './escola/escola.component';
+import { GeralComponent } from './escola/geral/geral.component';
+import { IndicesComponent } from './escola/indices/indices.component';
+import { ComentariosComponent } from './escola/comentarios/comentarios.component';
+import { ContatoComponent } from './escola/contato/contato.component';
+import { OnboardingComponent } from './onboarding/onboarding.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResultadoComponent
+    ResultadoComponent,
+    EscolaComponent,
+    GeralComponent,
+    IndicesComponent,
+    ComentariosComponent,
+    ContatoComponent,
+    OnboardingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
